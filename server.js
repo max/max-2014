e413706f3cf7a0d82994ed683576e9948293abbe
Max Schoening <mschoening@me.com>
2013-07-20 00:03:25 -0700

    Try maxAge

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var express = require('express');
 var app = express();
 
 app.use(express.logger('dev'));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public', { maxAge: 3600 }));
 app.listen(process.env.PORT || 5000);
