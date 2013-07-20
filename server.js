fec93a6065a863886fba0475d9a6564ed6b587ea
Max Schoening <mschoening@me.com>
2013-07-20 00:07:16 -0700

    Longer maxAge

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var express = require('express');
 var app = express();
 
 app.use(express.logger('dev'));
-app.use(express.static(__dirname + '/public', { maxAge: 3600 }));
+app.use(express.static(__dirname + '/public', { maxAge: 86400000 }));
 app.listen(process.env.PORT || 5000);
