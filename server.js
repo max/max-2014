cdb08d90e447dd417b2ce30a24635d938314580f
Max Schoening <mail@maximilian.io>
2014-02-10 21:42:00 +0000

    Turn into harp.js app

--- server.js
+++
@@ -1,6 +0,0 @@
-var connect = require('connect');
-
-connect.createServer(
-  connect.logger(),
-  connect.static(__dirname + '/public', {maxAge: 1000*60*60*24})
-).listen(process.env.PORT || 5000);
